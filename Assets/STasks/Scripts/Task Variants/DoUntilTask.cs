8cd8fa40b67e22d33f3e1689c0f84d2f599b6d3b
Jeremy Schiff <jeremyalexanderschiff@gmail.com>
2020-09-05 19:32:35 -0400

    Make DoUntil live up to its name
    My understanding of DoUntil is that it should keep doing the action every frame UNTIL some condition is true. This change makes DoUntil behave that way. Currently it seems to be a clone of DoWhenTask.

--- Assets/STasks/Scripts/Task Variants/DoUntilTask.cs
+++ Assets/STasks/Scripts/Task Variants/DoUntilTask.cs
@@ -8,14 +8,23 @@
         {
             condition = settings.condition;
         }
+        
+        protected override float GetProgress()
+        {
+            Debug.LogWarning($"{nameof(DoUntilTask)} can't keep track of the progress, you'll need to write your own implementation for this. Returning -1.");
+            return -1;
+        }
 
         protected override void OnUpdate(float deltaTime)
         {
             base.OnUpdate(deltaTime);
 
-            if (condition())
+            if (condition()) {
+              action.Invoke();
+            }
+            else
             {
-                Complete();
+                Kill();
             }
         }
     }
